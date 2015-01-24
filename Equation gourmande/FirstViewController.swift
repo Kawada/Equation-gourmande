9c49e2a76eef94b8a3b76bff60b258266ce85694
dleguis <dleguis@yahoo.fr>
2015-01-24 17:29:06 +0100

    Ajout d'une image redimensionnable automatiquement

--- Equation gourmande/FirstViewController.swift
+++ Equation gourmande/FirstViewController.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+// import Foundation
+
 
 class FirstViewController: UIViewController {
     
-    @IBOutlet var photoImageView: UIImageView!
+    
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet var descriptionTextView: UITextView!
+    @IBOutlet var photoImageView: UIImageViewModeScaleAspect!
+    
+    var myImage : UIImageViewModeScaleAspect!;
     
 
     override func viewDidLoad() {
@@ -20,13 +25,39 @@ class FirstViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         titleLabel.text = "BLA";
-        descriptionTextView.text = "Bla description";
+        descriptionTextView.text = "Bla description"
 
-        
+        NSLog("Image disponible ?")
         if let image = UIImage(named: "photo_main_example") {
-            photoImageView?.image = image;
+            NSLog("oui")
+            NSLog("Tentative d'ajout de l'image")
+            photoImageView.image = image
         }
         
+        myImage = UIImageViewModeScaleAspect(frame: CGRectMake(self.view.frame.minX, self.view.frame.minY, self.view.frame.size.width, self.view.frame.size.height / 2))
+        myImage.contentMode = .ScaleAspectFill // Add the first contentMode
+        myImage.image = UIImage(named: "photo_main_example")
+        myImage.backgroundColor = UIColor.blackColor()
+        self.view.addSubview(myImage)
+        
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: Selector("animateImage:"))
+        self.view.addGestureRecognizer(tap);
+    }
+    
+    func animateImage(recognizer:UITapGestureRecognizer) {
+        ////////////////////////
+        //
+        // Use automatic function to animate
+        //
+        ////////////////////////
+        
+        if myImage.contentMode == .ScaleAspectFit {
+            myImage.animateToScaleAspectFillToFrame(CGRectMake(self.view.frame.minX, self.view.frame.minY, self.view.frame.size.width, self.view.frame.size.height / 2), withDuration: 0.4, afterDelay: 0.0)
+        }
+        else {
+            myImage.animateToScaleAspectFitToFrame(CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height), withDuration: 0.4, afterDelay: 0.0)
+        }
     }
 
     override func didReceiveMemoryWarning() {
